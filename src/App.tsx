b3149bdda732ba7a90760bedccd19d5596f12277
indracb-dev19 <indra.cb.dev19@gmail.com>
2025-12-12 20:32:39 +0700

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import { Route, Routes } from "react-router";
 import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <>
-      <HomePage />
+      <Routes>
+        {/* <Route path="/" element={<HomePage />} /> */}
+        <Route index element={<HomePage />} />
+        <Route path="/checkout" />
+      </Routes>
     </>
   );
 }
