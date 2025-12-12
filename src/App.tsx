39a7660201ea6448b476cab7f077c6254305cdbd
indracb-dev19 <indra.cb.dev19@gmail.com>
2025-12-12 21:02:48 +0700

    move checkout page to react

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router";
 import HomePage from "./pages/HomePage";
+import CheckoutPage from "./pages/CheckoutPage";
 
 function App() {
   return (
@@ -7,7 +8,7 @@ function App() {
       <Routes>
         {/* <Route path="/" element={<HomePage />} /> */}
         <Route index element={<HomePage />} />
-        <Route path="/checkout" />
+        <Route path="/checkout" element={<CheckoutPage />} />
       </Routes>
     </>
   );
